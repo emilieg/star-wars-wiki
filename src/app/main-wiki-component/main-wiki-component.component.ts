5a663a45ab4f1584387d9360f057d856846054e8
emilieg <emilie.gerber@hotmail.com>
2017-12-10 12:01:23 -0800

    css and responsive

--- src/app/main-wiki-component/main-wiki-component.component.ts
+++ src/app/main-wiki-component/main-wiki-component.component.ts
@@ -22,10 +22,13 @@ export class MainWikiComponentComponent implements OnInit {
 
   public noResults:boolean; 
 
+  public wikiLink:string = 'http://starwars.wikia.com/wiki/Special:Search?query=';
+
   ngOnInit() {
     
     /* on initialization of component get all of the people */
     this.getAll();
+
 
     /* subscribe to the search Results from the Search Component and update the view*/
     this.sharedService.searchResults
@@ -46,11 +49,12 @@ export class MainWikiComponentComponent implements OnInit {
 
   private assignPeople(response){
     
-    console.log("assigning People", response);
 
       if(response.count > 0) {
         
         this.people = response.results;
+
+        this.createLink(this.people);
 
         this.checkForNextPage(response);
 
@@ -113,5 +117,39 @@ export class MainWikiComponentComponent implements OnInit {
     }
   
   }
+/*creates an external link to the Star Wars wiki search results for the selected person*/
+  private createLink(people){
+    
+    people.forEach((person) => {
+    
+      if(person.name.indexOf(' ') > -1) {
+    
+        let searchTerm = person.name.split(' ');
+    
+        for(let i = 0; i < searchTerm.length; i++){
+    
+          if(i === 0){
+    
+            person.a = this.wikiLink + searchTerm[i]
+    
+          } else {
+    
+            person.a += '+' + searchTerm[i];
+    
+          }
+    
+        }
+    
+      } else {
+    
+        person.a = this.wikiLink + person.name        
+    
+      }
+    
+    })
+  
+  }
+
+
 
 }
